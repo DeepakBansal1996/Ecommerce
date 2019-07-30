bdeb6f2362f1d12c121a2b57ced3533d9d182750
deepak9634349532 <deepak.bansal@cygrp.com>
2019-07-30 15:58:40 +0530

    compelete Auth Api work

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser')
+const expressValidator = require('express-validator');
+require('dotenv').config();
+
+// import routes
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+
+
+// app
+const app = express();
+
+// db
+mongoose.connect(process.env.DATABASE,{
+    useNewUrlParser: true,
+    useCreateIndex:true
+})
+.then(() => console.log("DB Connected"));
+
+//middleware
+app.use(morgan('dev'))
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+
+
+// routes middleware
+app.use('/api', authRoutes);
+app.use('/api', userRoutes);
+
+
+app.get('/', (req,res) =>{
+    res.send('hello from nodeJS');
+});
+
+const port = process.env.port || 8000;
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+});
+
